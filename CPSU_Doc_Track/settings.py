f18f928240149356146ebf4a0ff5ded3df280d00
John Kenneth Salgado <salgadojohnkenneth98@gmail.com>
2026-04-14 15:07:06 +0800

    checking

--- CPSU_Doc_Track/settings.py
+++ CPSU_Doc_Track/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#cokou4sf%l$k1c=sakak=4pp$!7zasp0!e$kr$!6vts6$t7-b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','cpsu-doc-track-1.onrender.com']
 
